a0b9fedee12bfb3c3e07d636e58a7a9c816923e1
Suresh-k <suresh091102@gmail.com>
2026-09-07 22:20:14 +0530

    settings change

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://whatsapp-1-wunu.onrender.com",
+    "https://convox-v1.onrender.com",
 ]
 
 if os.getenv("FRONTEND_URL"):
@@ -79,7 +79,7 @@ if os.getenv("FRONTEND_URL"):
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://whatsapp-1-wunu.onrender.com",
+    "https://convox-v1.onrender.com",
 ]
 
 if os.getenv("FRONTEND_URL"):
